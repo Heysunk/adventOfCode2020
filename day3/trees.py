5534ba56427a3565572492a180db513fc6323ef3
Michael Chlebek <michael.chlebek@gmail.com>
2020-12-04 17:04:27 +0100

    Done with day3.2

--- day3/trees.py
+++ day3/trees.py
@@ -1,23 +1,34 @@
 
+
+
+def calcHitTrees(input_lines, yMove, xMove):
+
+    xSize = len(input_lines[0]) - 1
+    ySize = len(input_lines)
+    x = 0
+    y = 0
+
+    treesHit = 0
+    while(y < ySize):
+        if (input_lines[y][x] == '#'):
+            treesHit += 1
+        x = (xMove + x) % xSize
+        y += yMove
+
+    return treesHit
+
+
+
+
 input_lines = []
 with open("input.txt") as file_in:
     for line in file_in:
         input_lines.append(line)
-xSize = len(input_lines[0]) - 1
-ySize = len(input_lines)
-x = 0
-y = 0
 
-treesHit = 0
-yMove = 1
-xMove = 3
-while(y < ySize):
-    if (input_lines[y][x] == '#'):
-        treesHit += 1
-    x = (xMove + x) % xSize
-    y += yMove
+prod = calcHitTrees(input_lines, 1,1)
+prod *= calcHitTrees(input_lines, 1,3)
+prod *= calcHitTrees(input_lines, 1,5)
+prod *= calcHitTrees(input_lines, 1,7)
+prod *= calcHitTrees(input_lines, 2,1)
 
-print(treesHit)
-
-
-
+print(prod)
